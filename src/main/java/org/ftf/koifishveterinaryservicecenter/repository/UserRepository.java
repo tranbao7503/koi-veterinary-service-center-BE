dea3bafb43f5329d2fbb0e8d3eefee23f401dbaa
Duong Trung Kien <kienduong2610@gmail.com>
2024-09-25 18:12:33 +0700

    View staff/veterinarian account list

--- src/main/java/org/ftf/koifishveterinaryservicecenter/repository/UserRepository.java
+++ src/main/java/org/ftf/koifishveterinaryservicecenter/repository/UserRepository.java
@@ -2,6 +2,11 @@ package org.ftf.koifishveterinaryservicecenter.repository;
 
 import org.ftf.koifishveterinaryservicecenter.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
-  }
+   List<User> findAllByRoleRoleId (Integer roleID);
+ }
---
+++ src/main/java/org/ftf/koifishveterinaryservicecenter/service/ManagerService.java
@@ -0,0 +1,38 @@
+package org.ftf.koifishveterinaryservicecenter.service;
+
+import org.ftf.koifishveterinaryservicecenter.dto.UserDTO;
+import org.ftf.koifishveterinaryservicecenter.entity.User;
+import org.ftf.koifishveterinaryservicecenter.repository.UserRepository;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeMap;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+@Service
+public class ManagerService {
+
+    private final UserRepository userRepository;
+    private final ModelMapper modelMapper;
+
+    public ManagerService(UserRepository userRepository, ModelMapper modelMapper) {
+        this.userRepository = userRepository;
+        this.modelMapper = modelMapper;
+    }
+
+    public UserDTO convertToUserDTO(User user) {
+        TypeMap<User, UserDTO> typeMap = modelMapper.getTypeMap(User.class, UserDTO.class);
+        if (typeMap == null) {
+            typeMap = modelMapper.createTypeMap(User.class, UserDTO.class);
+        }
+        return typeMap.map(user);
+    }
+
+    public List<UserDTO> getAllUser(int roleId) {
+        List<User> users = userRepository.findAllByRoleRoleId(roleId);
+        List<UserDTO> userDtos = users.stream()
+                .map(this::convertToUserDTO)
+                .collect(Collectors.toList());
+        return userDtos;
+    }
+}
