c9140621ddeaeeb29f5e70243709b8395cf909a1
Duong Trung Kien <kienduong2610@gmail.com>
2024-09-25 17:55:28 +0700

    update staff/veterinarian account

--- src/main/java/org/ftf/koifishveterinaryservicecenter/entity/User.java
+++ src/main/java/org/ftf/koifishveterinaryservicecenter/entity/User.java
@@ -1,11 +1,15 @@
 package org.ftf.koifishveterinaryservicecenter.entity;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 import org.hibernate.annotations.ColumnDefault;
 
 import java.util.LinkedHashSet;
 import java.util.Set;
 
+@Getter
+@Setter
 @Entity
 @Table(name = "users")
 public class User {
--- src/main/java/org/ftf/koifishveterinaryservicecenter/repository/UserRepository.java
+++ src/main/java/org/ftf/koifishveterinaryservicecenter/repository/UserRepository.java
@@ -2,6 +2,8 @@ package org.ftf.koifishveterinaryservicecenter.repository;
 
 import org.ftf.koifishveterinaryservicecenter.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
   }
