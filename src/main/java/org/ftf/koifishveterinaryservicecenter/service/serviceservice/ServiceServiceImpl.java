bbffe217713b673e9c9a8620828b58e7648c965d
Bao Pen <tranquocbao07052003@gmail.com>
2024-09-27 08:43:29 +0700

    update: service mapper

--- src/main/java/org/ftf/koifishveterinaryservicecenter/service/serviceservice/ServiceServiceImpl.java
+++ src/main/java/org/ftf/koifishveterinaryservicecenter/service/serviceservice/ServiceServiceImpl.java
@@ -15,25 +15,19 @@ import java.util.stream.Collectors;
 public class ServiceServiceImpl implements ServiceService {
 
 
-
     private final ServiceRepository serviceRepository;
-    private final ServiceMapper serviceMapper;
 
     @Autowired
-    public ServiceServiceImpl(ServiceRepository serviceRepository, ServiceMapper serviceMapper) {
+    public ServiceServiceImpl(ServiceRepository serviceRepository) {
         this.serviceRepository = serviceRepository;
-        this.serviceMapper = serviceMapper;
     }
 
     /*
      * Get all available services
      * */
-    public List<ServiceDTO> getAllServices() {
+    public List<Service> getAllServices() {
         List<Service> services = serviceRepository.findAll();
-        List<ServiceDTO> serviceDTOs = services.stream()
-                .map(serviceMapper::convertToServiceDTO)
-                .collect(Collectors.toList());
-        return serviceDTOs;
+        return services;
     }
 
 
@@ -49,18 +43,15 @@ public class ServiceServiceImpl implements ServiceService {
      * Update price of service
      * */
     @Override
-    public ServiceDTO updateService(Integer serviceId, ServiceDTO serviceFromRequest) throws AppointmentServiceNotFoundException {
-
-        // convert dto -> entity
-        Service convertedService = serviceMapper.convertToService(serviceFromRequest);
+    public Service updateService(Integer serviceId, Service serviceFromRequest) throws AppointmentServiceNotFoundException {
 
         // check service existed from db
         Service serviceFromDb = serviceRepository.findById(serviceId).orElse(null);
         if (serviceFromDb == null) {
             throw new AppointmentServiceNotFoundException("Service not found with ID: " + serviceId);
         }
-        ServiceDTO result = serviceMapper.convertToServiceDTO(serviceRepository.save(convertedService));
-        return result;
+        serviceFromDb = serviceRepository.save(serviceFromRequest);
+        return serviceFromDb;
     }
 
 }
