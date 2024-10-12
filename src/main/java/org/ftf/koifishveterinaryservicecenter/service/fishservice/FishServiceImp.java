f4825bce0fdc16d6a93635a553142fe25acf5316
Duong Trung Kien <kienduong2610@gmail.com>
2024-10-12 11:32:32 +0700

    xong delete fish

--- src/main/java/org/ftf/koifishveterinaryservicecenter/service/fishservice/FishServiceImp.java
+++ src/main/java/org/ftf/koifishveterinaryservicecenter/service/fishservice/FishServiceImp.java
@@ -2,9 +2,11 @@ package org.ftf.koifishveterinaryservicecenter.service.fishservice;
 
 import org.ftf.koifishveterinaryservicecenter.dto.FishDTO;
 import org.ftf.koifishveterinaryservicecenter.entity.Fish;
+import org.ftf.koifishveterinaryservicecenter.exception.AuthenticationException;
 import org.ftf.koifishveterinaryservicecenter.exception.FishNotFoundException;
 import org.ftf.koifishveterinaryservicecenter.mapper.FishMapper;
 import org.ftf.koifishveterinaryservicecenter.repository.FishRepository;
+import org.ftf.koifishveterinaryservicecenter.service.userservice.AuthenticationServiceImpl;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -13,10 +15,12 @@ import java.util.List;
 public class FishServiceImp implements FishService {
     private final FishRepository fishRepository;
     private final FishMapper fishMapper;
+    private final AuthenticationServiceImpl authenticationService;
 
-    public FishServiceImp(FishRepository fishRepository, FishMapper fishMapper) {
+    public FishServiceImp(FishRepository fishRepository, FishMapper fishMapper, AuthenticationServiceImpl authenticationService) {
         this.fishRepository = fishRepository;
         this.fishMapper = fishMapper;
+        this.authenticationService = authenticationService;
     }
 
 
@@ -35,18 +39,28 @@ public class FishServiceImp implements FishService {
     //Xoa ca khoi list
     @Override
     public FishDTO removeFish(int fishId, boolean enabled) {
-        // Lấy thông tin người dùng từ database
+        // Lấy thông tin con cá từ database
         Fish fishFromDb = fishRepository.findById(fishId).orElse(null);
 
-
         if (fishFromDb == null) {
-            throw new FishNotFoundException("User not found with ID: " + fishId);
+            throw new FishNotFoundException("Fish not found with ID: " + fishId);
+        }
+
+        // Lấy customerId từ fish
+        int fishCustomerId = fishFromDb.getCustomer().getUserId();
+
+        // Lấy customerId từ token
+        int loggedInCustomerId = authenticationService.getAuthenticatedUserId(); // Sử dụng phương thức của bạn
+
+        // So sánh customerId của fish với customerId của người dùng đăng nhập
+        if (fishCustomerId != loggedInCustomerId) {
+            throw new AuthenticationException("You can only remove your own fish.");
         }
 
         // Cập nhật enable/disable cho fish
         fishFromDb.setEnabled(enabled);
 
-        // Lưu người dùng đã cập nhật
+        // Lưu cá đã cập nhật
         Fish updatedFish = fishRepository.save(fishFromDb);
 
         // Sử dụng mapper để chuyển đổi entity sang DTO
